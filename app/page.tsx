985f5d1e5013ebf01688e6f27da0192a1d686dc9
Kevin La <chenchiaho@gmail.com>
2023-09-23 22:46:55 +0800

    add hero image and animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 // import SEO from "@/components/SEO"
 
-
-
 const Home = async ({ params }: { params: { id: string } }) => {
 
     return (
 
         <>
+
             {/* <SEO
                 // seoData={apiGlobal.data.attributes.Seo}
                 ogType="website"
