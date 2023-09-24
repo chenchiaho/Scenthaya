c40bc00c9e56e57a9d2835671a075281df205f57
Kevin La <chenchiaho@gmail.com>
2023-09-24 17:15:58 +0800

    fix image parent height problem

--- components/heros/Hero.tsx
+++ components/heros/Hero.tsx
@@ -1,42 +1,30 @@
-'use client'
-import { motion, useAnimation } from 'framer-motion';
-import Image from 'next/image';
-import { useEffect } from 'react';
+import Image from 'next/image'
 
 const Hero = () => {
-    const controls = useAnimation();
-
-    useEffect(() => {
-        const handleScroll = () => {
-            let yOffset = window.scrollY;
-            controls.start({ y: yOffset * 0.2 });  // 0.2 determines the "speed" of the parallax effect
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, [controls]);
 
     return (
-        <div className="relative w-full h-[100vh]">
-            <div className="z-10 absolute w-full h-full">
-                <Image
-                    src="/images/home-2/product-0.png"
-                    alt="product 0"
-                    layout='fill'
-                    objectFit='contain'
-                    objectPosition='top center'
-                />
+        <>
+            <div className="relative w-full pb-[56.25%]">
+                <div className="absolute w-full h-full">
+                    <Image
+                        src="/images/home-2/product-0.png"
+                        alt="product 0"
+                        layout='fill'
+                        objectFit='contain'
+                        objectPosition='top center'
+                    />
+                </div>
+                <div
+                    className="absolute inset-0 flex items-center justify-center font-bold text-black px-6 whitespace-nowrap"
+                    style={{ fontSize: "clamp(12px, 10vw, 10rem)" }}
+                >
+                    Incredible Sound
+                </div>
             </div>
+        </>
 
-            <motion.div
-                animate={controls}
-                className="absolute inset-0 flex items-center justify-center font-bold z-0"
-                style={{ fontSize: "clamp(16px, 12vw, 10rem)" }}
-            >
-                Incredible Sound
-            </motion.div>
-        </div>
     )
 }
 
-export default Hero;
+export default Hero
+
