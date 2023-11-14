cf5ebbaa01f932b6f50566d49812cce074657279
Circle <ja95175@gmail.com>
2023-11-14 17:38:25 +0800

    Modified ButtonClose

--- shared/ButtonClose/ButtonClose.tsx
+++ shared/ButtonClose/ButtonClose.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { XMarkIcon } from "@heroicons/react/24/solid";
-import twFocusClass from "@/utils/twFocusClass";
 
 export interface ButtonCloseProps {
   className?: string;
@@ -15,13 +14,9 @@ const ButtonClose: React.FC<ButtonCloseProps> = ({
 }) => {
   return (
     <button
-      className={
-        `w-8 h-8 flex items-center justify-center rounded-full text-neutral-700 dark:text-neutral-300 hover:bg-neutral-100 dark:hover:bg-neutral-700 ${className} ` +
-        twFocusClass()
-      }
+      className={`h-8 w-8 flex justify-center items-center rounded-full hover:bg-gray-500 ${className} `}
       onClick={onClick}
     >
-      <span className="sr-only">Close</span>
       <XMarkIcon className={IconclassName} />
     </button>
   );
